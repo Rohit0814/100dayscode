ae2247b06fef0ba2f8b5cc8a930accb235a161d7
rohit0814 <singhrohit.rs747@gmail.com>
2024-01-31 22:46:23 +0530

    Successfully completed the day12 task

---
+++ day12/script.js
@@ -0,0 +1,62 @@
+// var clcikBtn=document.getElementById('click');
+
+// var inputText=document.getElementById('inputText');
+// var inputLabel=document.getElementById('inputLabel');
+
+// clcikBtn.addEventListener('mouseover',function(){
+//     clcikBtn.style.backgroundColor="red";
+//     clcikBtn.style.color="white"
+// });
+
+// clcikBtn.addEventListener('mouseout',function(){
+//     clcikBtn.style.backgroundColor="rgb(193, 193, 193)";
+//     clcikBtn.style.color="black";
+// });
+
+// clcikBtn.addEventListener('click',function(){
+//     var text=inputText.value;
+//     inputLabel.innerHTML=text;
+// });
+
+// var text2="";
+
+// // inputText.addEventListener('keypress',function(event){
+// //     text2=String.fromCharCode(event.keyCode);
+// //     inputLabel.innerHTML=text2;
+// // });
+
+// inputText.addEventListener('keyup',function(event){
+//     text2=String.fromCharCode(event.keyCode);
+//     inputLabel.innerHTML=text2;
+// });
+
+// var outer = document.getElementById('outer');
+
+// outer.addEventListener('mouseover',function(){
+//     outer.style.backgroundColor="green";
+// });
+
+// outer.addEventListener('mouseout',function(){
+//     outer.style.backgroundColor="red";
+// });
+
+// var inner = document.getElementById('inner');
+
+// inner.addEventListener('mouseover',function(event){
+//     event.stopPropagation();
+//     inner.style.backgroundColor="cyan";
+// });
+
+// inner.addEventListener('mouseout',function(){
+//     inner.style.backgroundColor="blue";
+// });
+
+"use strict";
+
+function x(){
+    let a=5;
+    console.log(a);
+}
+x();
+
+
